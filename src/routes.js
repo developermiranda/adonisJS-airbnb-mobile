b42dd37367dcceb13fd3a8ec0203191fc3682859
Daniel Miranda <developermiranda@gmail.com>
2019-11-27 11:27:42 -0300

    Iniciando com React Native: Navegacao e JWT

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+// import { createStackNavigator } from 'react-navigation';
+
+// import SignIn from './pages/signIn';
+// import SignUp from './pages/signUp';
+// import Main from './pages/main';
+
+// const Routes = createStackNavigator({
+//   SignIn,
+//   SignUp,
+//   Main,
+// });
+
+// export default Routes;
+
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import SignIn from './pages/signIn';
+// import SignUp from './pages/signUp';
+import Main from './pages/main';
+
+const Routes = createStackNavigator({
+  SignIn,
+  // SignUp,
+  Main,
+});
+
+const RoutesCtn = createAppContainer(Routes);
+
+export default RoutesCtn;
